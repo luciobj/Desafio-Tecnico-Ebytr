62ce1b4a07ebd3133ffc61a7624771d5e6f97f58
Lúcio BJ <lbjunq@hotmail.com>
2022-02-16 18:49:49 -0300

    feat: added put route

--- backend/router/index.js
+++ backend/router/index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const {
   findAllTasksController,
-  createTaskController
+  createTaskController,
+  updateTaskController
 } = require('../controllers/taskControllers');
 
 const router = express.Router();
 
 router.get('/task', findAllTasksController);
 router.post('/task', createTaskController);
+router.put('/task/:id', updateTaskController);
 
 module.exports = router;
