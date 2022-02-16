725c89e49b45065fe60f1ae248bba0bdbc7c6bf1
Lúcio BJ <lbjunq@hotmail.com>
2022-02-16 18:48:32 -0300

    feat: added task update service, find by id model

---
+++ backend/services/taskUpdate.js
@@ -0,0 +1,13 @@
+const errorConstructor = require('../util/functions/errorConstructor');
+const { badRequest, notFound } = require('../util/library/statusCode');
+const updateTask = require('../models/updateTask');
+const { ObjectId } = require('mongodb');
+const findTaskById = require('../models/findTaskById');
+
+module.exports = async (id, status) => {
+  if (!ObjectId(id)) throw errorConstructor(badRequest, 'Invalid id');
+  if (!findTaskById(id)) throw errorConstructor(notFound, 'Task not found');
+  if (!status) throw errorConstructor(badRequest, 'Missing or empty status');
+  await updateTask(id, status);
+  return 'Done';
+};
