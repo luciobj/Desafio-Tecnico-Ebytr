08aa2430b9612539e4788ab0d1b06e3eaa8ecf31
Lúcio BJ <lbjunq@hotmail.com>
2022-02-16 17:18:29 -0300

    test: updated taskList service file

--- backend/tests/services/tasksList.test.js
+++ backend/tests/services/tasksList.test.js
@@ -1,15 +1,22 @@
 const sinon = require('sinon');
 const { expect } = require('chai');
-const taskList = require('../../services/taskList');
+const taskList = require('../../services/tasksList');
 const { MongoClient } = require('mongodb');
 const connectionMemory = require('../../util/functions/connectionMemory');
 
-describe('Tests the list all tasks', () => {
+const dbName = 'ToDoList';
+const collection = 'tasks';
+
+describe('Tests the list all tasks service', () => {
+  let connectionMock;
+
   before(async () => {
-    const connectionMock = await connectionMemory();
+    connectionMock = await connectionMemory();
     sinon.stub(MongoClient, 'connect').resolves(connectionMock);
   });
   after(() => {
+    // await connectionMock.db(dbName).collection(collection).drop();
+    MongoClient.connect.restore();
     sinon.reset();
   });
 
