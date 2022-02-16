1a270ff176b2dde2ae45aa3cc4c4384ac1c819a1
Lúcio BJ <lbjunq@hotmail.com>
2022-02-16 17:13:13 -0300

    test: updated findAllTasks test

--- backend/tests/models/findAllTasks.test.js
+++ backend/tests/models/findAllTasks.test.js
@@ -1,16 +1,22 @@
 const sinon = require('sinon');
 const { expect } = require('chai');
 const findAllTasks = require('../../models/findAllTasks');
-const createTask = require('../../models/createTask');
 const { MongoClient } = require('mongodb');
 const connectionMemory = require('../../util/functions/connectionMemory');
 
+const dbName = 'ToDoList';
+const collection = 'tasks';
+
 describe('Tests the list all tasks model', () => {
+  let connectionMock;
+
   before(async () => {
-    const connectionMock = await connectionMemory();
+    connectionMock = await connectionMemory();
     sinon.stub(MongoClient, 'connect').resolves(connectionMock);
   });
-  after(() => {
+  after(async () => {
+    await connectionMock.db(dbName).collection(collection).drop();
+    MongoClient.connect.restore();
     sinon.reset();
   });
 
@@ -24,14 +30,17 @@ describe('Tests the list all tasks model', () => {
       const task = {
         name: 'Clean the office',
         status: 'Pending',
-      }
-      await createTask(task);
+        createdAt: ''
+      };
+      await connectionMock.db(dbName).collection(collection).insertOne(task);
+      await connectionMock.db(dbName).collection(collection).updateOne({ name: task.name }, { $currentDate: { createdAt: true } });
       const findResult = await findAllTasks();
       expect(findResult).to.be.a('array');
       expect(findResult).to.be.length(1);
+      expect(findResult[0]._id).to.exist;
       expect(findResult[0].name).to.be.eq(task.name);
       expect(findResult[0].status).to.be.eq(task.status);
-      expect(findResult[0].createdAt).to.be.exist();
+      expect(findResult[0].createdAt).to.exist;
     });
   });
 });
