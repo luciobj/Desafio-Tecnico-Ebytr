c7b7e9052388968f18be86863ce3517d72c64d62
Lúcio BJ <lbjunq@hotmail.com>
2022-02-16 18:21:52 -0300

    feat: added create task controller, fix try catch

--- backend/controllers/taskControllers.js
+++ backend/controllers/taskControllers.js
@@ -1,11 +1,28 @@
+const taskCreate = require("../services/taskCreate");
 const tasksList = require("../services/tasksList");
 const { success } = require("../util/library/statusCode");
 
-const findAllTasksController = async (_request, response, _next) => {
-  const list = await tasksList();
-  return response.status(success).json(list);
+const findAllTasksController = async (_request, response, next) => {
+  try {
+    const list = await tasksList();
+    return response.status(success).json(list);
+  } catch (error) {
+    console.log('GET ALL TASKS:', error);
+    return next(error);
+  }
+};
+const createTaskController = async (request, response, next) => {
+  try {
+    const task = request.body;
+    const createdObj = await taskCreate(task);
+    return response.status(success).json(createdObj);
+  } catch (error) {
+    console.log('CREATE TASK:', error);
+    return next(error);
+  }
 };
 
 module.exports = {
-  findAllTasksController
+  findAllTasksController,
+  createTaskController
 };
