342a4af6eed33c28275c8df08f64f5d06497b83b
Lúcio BJ <lbjunq@hotmail.com>
2022-02-16 20:22:18 -0300

    feat: added components on home

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,7 +1,15 @@
+import AddForm from "../components/AddForm";
+import TaskCard from "../components/TaskCard";
+
 function Home() {
   return (
     <div className="Home">
-      Home
+      <h2 className='Title'>
+        To Do List
+      </h2>
+      {AddForm()}
+      <h3>Tasks</h3>
+      {TaskCard('Limpar', 'Pendente', '2022')}
     </div>
   );
 }
