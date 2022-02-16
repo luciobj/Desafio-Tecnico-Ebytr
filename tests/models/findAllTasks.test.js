4725b90b2f4e8c35fed669a0ad4ceb27715b9c27
Lúcio BJ <lbjunq@hotmail.com>
2022-02-16 14:52:25 -0300

    fix: added asynchronicity on connection and test

--- tests/models/findAllTasks.test.js
+++ tests/models/findAllTasks.test.js
@@ -4,8 +4,8 @@ const findAllTasks = require('../../models/findAllTasks');
 
 describe('Tests the list all tasks', () => {
   describe('Tests the success case', () => {
-    it('returns a array', () => {
-      const findResult = findAllTasks();
+    it('returns a array', async () => {
+      const findResult = await findAllTasks();
       expect(findResult).to.be.a('array');
     });
   });
